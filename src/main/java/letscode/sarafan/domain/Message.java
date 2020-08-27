b7c940040876944a9febd27036cdb51a044d0a22
Skazochnik-san <lera.korchagina.1998@mail.ru>
2020-08-27 21:49:00 +0300

    Integrate tha database through JPA

---
+++ src/main/java/letscode/sarafan/domain/Message.java
@@ -0,0 +1,33 @@
+package letscode.sarafan.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@ToString(of = {"id", "text"})
+@EqualsAndHashCode(of = {"id"})
+public class Message {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+    private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
---
+++ src/main/java/letscode/sarafan/repos/MessageRepo.java
@@ -0,0 +1,7 @@
+package letscode.sarafan.repos;
+
+import letscode.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepo extends JpaRepository<Message, Integer> {
+}
